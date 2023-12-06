35306306185585ff797b853e0a7ac5092e187201
devhoya97 <diajbh123@gmail.com>
2023-12-06 18:50:54 +0900

    feat: 경주할 자동차 이름을 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,14 +1,33 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
+    private static final String DELIMITER = ",";
+
     public static List<String> getCarNames() {
-        return null;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        String[] splitInput = Console.readLine().split(DELIMITER);
+        List<String> carNames = new ArrayList<>();
+
+        for (String input : splitInput) {
+            carNames.add(input.trim());
+        }
+
+        validateEmpty(carNames);
+
+        return carNames;
     }
 
-    private static void validateEmpty(List<String> inputs) {
-
+    private static void validateEmpty(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     public static int getTryCount() {
