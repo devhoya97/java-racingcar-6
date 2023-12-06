f3fb2c10e55fb2c44877180b9c626bd480e4e793
devhoya97 <diajbh123@gmail.com>
2023-12-06 18:15:26 +0900

    feat: 숫자 값에 따라 자동차를 이동시키는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,11 +2,15 @@ package racingcar;
 
 public class Car {
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_NUMBER_FOR_MOVE = 4;
+    private static final int DEFAULT_MOVE_COUNT = 0;
     private final String name;
+    private int moveCount;
 
     public Car(String name) {
         validateName(name);
         this.name = name;
+        moveCount = DEFAULT_MOVE_COUNT;
     }
 
     private void validateName(String name) {
@@ -16,7 +20,9 @@ public class Car {
     }
 
     public void move(int number) {
-
+        if (number >= MIN_NUMBER_FOR_MOVE) {
+            moveCount++;
+        }
     }
 
     public boolean isEqualMoveCount(int moveCount) {
