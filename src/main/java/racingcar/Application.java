9cc0f8d37b3eeb4ac3b90b58876937f313fca44b
devhoya97 <diajbh123@gmail.com>
2023-12-06 19:18:55 +0900

    feat: 게임을 진행하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,24 @@
 package racingcar;
 
-import java.util.List;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        CarGroup carGroup = new CarGroup(List.of("pobi", "jun"));
-        carGroup.move(new RandomNumberGenerator());
-        OutputView.printProgress(carGroup.getCars());
+        CarGroup carGroup = new CarGroup(InputView.getCarNames());
+        int tryCount = InputView.getTryCount();
 
-        carGroup.move(new RandomNumberGenerator());
-        OutputView.printProgress(carGroup.getCars());
-
-        carGroup.move(new RandomNumberGenerator());
-        OutputView.printProgress(carGroup.getCars());
+        OutputView.printMessageBeforeProgress();
+        play(carGroup, tryCount, new RandomNumberGenerator());
 
         OutputView.printWinner(Referee.findWinners(carGroup));
+    }
+
+    private static void play(CarGroup carGroup, int tryCount, RandomNumberGenerator randomNumberGenerator) {
+        for (int count = 0; count < tryCount; count++) {
+            carGroup.move(randomNumberGenerator);
+
+            OutputView.printProgress(carGroup.getCars());
+        }
     }
 }
